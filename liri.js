10c2b8857bbf4193a3cbacc4fa2f6f4a6bbb7d4e
cbrenner04 <christopher.brenner1@northwestern.edu>
2016-12-13 22:55:03 -0600

    Clean up and comment

--- liri.js
+++ liri.js
@@ -1,16 +1,24 @@
+// require keys document for twitter api use
 var keys = require('./keys.js');
+// require inquirer for getting user input at command line
 var inquirer = require('inquirer');
+// require twitter package
 var Twitter = require('twitter');
+// set up twitter
 var client = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret
 });
+// require spotify package
 var spotify = require('spotify');
+// require request package
 var request = require('request');
+// require fs library
 var fs = require('fs');
 
+// get user input
 inquirer.prompt([{
     name: 'command',
     message: 'What would you like to do?',
@@ -28,37 +36,60 @@ inquirer.prompt([{
         name: 'do-what-it-says',
         short: 'run the command in random.txt'
     }]
+// once user input is received
 }]).then(function(answer) {
+    // get tweets if 'my-tweets is selected'
     if (answer.command === 'my-tweets') {
         getTweets();
+    // get songs if 'spotify-this-song' is selected
     } else if (answer.command === 'spotify-this-song') {
         getSong();
+    // get movie if 'movie-this' is selected
     } else if (answer.command === 'movie-this') {
         getMovie();
+    // if 'do-what-it-says' is selected
     } else if (answer.command === 'do-what-it-says') {
+        // read the random.txt file for the command/parameters
         fs.readFile('./random.txt', 'utf8', function(error, data) {
             if (error) {
                 console.log('Error occurred: ' + error);
             }
 
+            // initialize variables at this level to use later
             var command;
             var parameter;
 
+            // if there are more than one argument in the random.txt file
             if (data.includes(',')) {
+                // split into array
                 var array = data.split(',');
+                // the command is the first argument
                 command = array[0];
+                // the parameter for the command is the second argument
                 parameter = array[1].replace('\n', '');
+            // otherwise its a single argument command
             } else {
                 command = data.replace('\n', '');
             }
 
+            // get tweets if 'my-tweets' is in the file
             if (command === 'my-tweets') {
                 getTweets();
+            // if 'spotify-this-song'
             } else if (command === 'spotify-this-song') {
-                getSong(parameter);
+                // if a paramater is included in the file
+                if (parameter) {
+                    getSong(parameter);
+                // otherwise this command is invalid
+                } else {
+                    console.log('Not a valid command.');
+                }
+            // if the command in the file is 'movie-this'
             } else if (command === 'movie-this') {
+                // if a parameter was given
                 if (parameter) {
                     getMovie(parameter);
+                // otherwise set the argument to an unlikely string so default is searched
                 } else {
                     getMovie('987654321abcdefghijklmnopqrstuvwxyz');
                 }
@@ -71,11 +102,14 @@ inquirer.prompt([{
     }
 });
 
+// tweets function
 function getTweets() {
+    // get most recent tweets for my username, up to 20 tweets
     client.get('statuses/user_timeline', {
         screen_name: 'cbrenner265',
         count: 20
     }, function(error, tweets, response) {
+        // if there was an error log it
         if (error) {
             console.log('Error occurred: ' + error);
             return;
@@ -83,12 +117,15 @@ function getTweets() {
 
         var output = '';
 
+        // append each tweet to the output string
         tweets.forEach(function(tweet) {
             output += 'text: ' + tweet.text + '\ncreated: ' + tweet.created_at + '\n';
         });
 
+        // console log the output
         console.log(output);
 
+        // append the command and the output to the log.txt file
         fs.appendFile('log.txt', 'my-tweets ' + '\n' + output + '\n', 'utf8', function(error) {
             if (error) {
                 console.log('Error occurred: ' + error);
@@ -97,9 +134,12 @@ function getTweets() {
     });
 }
 
+// get song function
 function getSong(song) {
+    // if a song was passed as an argument
     if (song) {
         searchSpotify(song);
+    // otherwise get the song from the user
     } else {
         inquirer.prompt([{
             name: 'song',
@@ -110,7 +150,9 @@ function getSong(song) {
     }
 }
 
+// search spotify function
 function searchSpotify(input) {
+    // make search of the track, limit it to the first since thats all we'll use anyway
     spotify.search({
         type: 'track',
         query: input + '&limit=1'
@@ -121,28 +163,35 @@ function searchSpotify(input) {
         }
 
         var track = data.tracks.items[0];
+
+        // set output string
         output = 'Artist: ' + track.artists[0].name + '\nName: ' + track.name +
             '\nPreview URL: ' + track.preview_url + '\nAlbum: ' + track.album.name;
 
+        // console log output
         console.log(output);
 
-        fs.appendFile('log.txt', 'spotify-this-song ' + '"' + input + '"' + '\n' +
-            output + '\n\n', 'utf8',
-            function(error) {
-                if (error) {
-                    console.log('Error occurred: ' + error);
-                }
-            });
+        // append command and output to log.txt file
+        fs.appendFile('log.txt', 'spotify-this-song ' + '"' + input + '"' + '\n' + output + '\n\n', 'utf8', function(error) {
+            if (error) {
+                console.log('Error occurred: ' + error);
+            }
+        });
     });
 }
 
+// get movie function
 function getMovie(movie) {
+    // if movie is passed as an argument
     if (movie) {
+        // if it is the unlikely string, search default
         if (movie === '987654321abcdefghijklmnopqrstuvwxyz') {
             searchOmdb('mr nobody');
+        // otherwise search for the movie given as argument
         } else {
             searchOmdb(movie);
         }
+    // otherwise get movie input from the user
     } else {
         inquirer.prompt([{
             name: 'movie',
@@ -154,32 +203,36 @@ function getMovie(movie) {
     }
 }
 
+// search omdb
 function searchOmdb(input) {
-    request('http://www.omdbapi.com/?' + 't=' + input.replace(' ', '+') +
-        '&type=movie&tomatoes=true',
-        function(error, response, data) {
-            if (error) {
-                console.log('Error occurred: ' + error);
-                return;
-            }
+    // hit the omdb api, input is title, type is movie, and we want rotten tomatoes info
+    request('http://www.omdbapi.com/?' + 't=' + input.replace(' ', '+') + '&type=movie&tomatoes=true', function(error, response, data) {
+        // if error occurs, log it
+        if (error) {
+            console.log('Error occurred: ' + error);
+            return;
+        }
 
-            if (!error && response.statusCode == 200) {
-                var movie = JSON.parse(data);
-                output = 'Title: ' + movie.Title + '\nYear: ' + movie.Year +
-                    '\nIMDB Rating: ' + movie.imdbRating + '\nCountry: ' + movie.Country +
-                    '\nLanguage: ' + movie.Language + '\nPlot: ' + movie.Plot + '\nActors: ' +
-                    movie.Actors + '\nRotten Tomatoes Rating: ' + movie.tomatoRating +
-                    '\nRotten Tomatoes URL: ' + movie.tomatoURL;
+        // if there is no error and we get a good status code back
+        if (!error && response.statusCode == 200) {
+            // parse the movie data so we can easily deal with it
+            var movie = JSON.parse(data);
+            // set output string
+            output = 'Title: ' + movie.Title + '\nYear: ' + movie.Year +
+                '\nIMDB Rating: ' + movie.imdbRating + '\nCountry: ' + movie.Country +
+                '\nLanguage: ' + movie.Language + '\nPlot: ' + movie.Plot + '\nActors: ' +
+                movie.Actors + '\nRotten Tomatoes Rating: ' + movie.tomatoRating +
+                '\nRotten Tomatoes URL: ' + movie.tomatoURL;
 
-                console.log(output);
+            // console log output
+            console.log(output);
 
-                fs.appendFile('log.txt', 'movie-this ' + '"' + input + '"' + '\n' +
-                    output + '\n\n', 'utf8',
-                    function(error) {
-                        if (error) {
-                            console.log('Error occurred: ' + error);
-                        }
-                    });
-            }
-        });
+            // append command and output to log.txt
+            fs.appendFile('log.txt', 'movie-this ' + '"' + input + '"' + '\n' + output + '\n\n', 'utf8', function(error) {
+                if (error) {
+                    console.log('Error occurred: ' + error);
+                }
+            });
+        }
+    });
 }
